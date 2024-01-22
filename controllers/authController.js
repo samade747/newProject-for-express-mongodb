113e5b03130c8080b7256de69850d397a0962f10
samade747 <samad.e747@gmail.com>
2024-01-22 23:36:55 +0500

    cxxccz

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
 import UsersSchema from '../models/user.js'
 
 export const signupController = async (req, res) => {
-    
+    res.json({
+                status: true,
+                message: "Sign up successfully",
+                data: []
+            })
 }
