5dbb5b9d7d3b57d00e55b04e6978a315a950988a
samade747 <samad.e747@gmail.com>
2024-01-22 15:22:33 +0500

    322

---
+++ controllers/authController.js
@@ -0,0 +1,5 @@
+import UsersSchema from '../models/user.js'
+
+export const signupController = async (req, res) => {
+    
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,1 +1,13 @@
-const authRoute = samad1
+import express from "express"
+import { forgetPasswordController, loginController, logoutController, signupController } from "../controllers/authController.js"
+
+// create a const variable = getting express.Router() 
+// function to route from app.use('/auth', authRoute)
+const authRoute = express.Router()
+
+authRoute.post("/signup", signupController)
+authRoute.post("/login", loginController)
+authRoute.post("/logout", logoutController)
+authRoute.put("/forgetPassword", forgetPasswordController)
+
+export { authRoute }
