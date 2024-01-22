cd0525cc86d7524de8ec82e7cab14d360eefc654
samade747 <samad.e747@gmail.com>
2024-01-22 13:59:42 +0500

    159pm

--- index.js
+++ index.js
@@ -1,11 +1,140 @@
-const express = require('express')
+import express from "express"
+import { authRoute } from "./routes/authRoute.js"
+import dotenv from 'dotenv'
+import { dbConnection } from "./utils/config.js"
+
 const app = express()
-const port = 3000
+dotenv.config()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dbConnection()
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.use('/auth', authRoute)
+// app.use("/jobs", jobsRoute)
+// app.use("/categories", categoriesRoute)
+// app.use("/filterations", filterationsRoute)
+// app.use("/users", usersRoute)
+// app.use("/blogs", blogsRoute)
+// app.use("/tutorials", tutorialsRoute)
+// app.use("/companies", companiesRoute)
+
+// app.post("auth/signup", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "Sign up successfully",
+//         data: []
+//     })
+// })
+// app.post("auth/login", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "Login in successfully",
+//         data: []
+//     })
+// })
+// app.post("auth/logout", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "Logout successfully"
+//     })
+// })
+// app.post("auth/forgetPassword", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "Password changed successfully"
+//     })
+// })
+// app.get("jobs", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "All Jobs Data get Successfully",
+//         data: []
+//     })
+// })
+// app.get("jobs/:jobId", (req, res) => {
+//     const { jobId } = req.params           //{jobId: "dadasdasda"}
+//     res.json({
+//         status: true,
+//         message: "Job Data get Successfully",
+//         data: {}
+//     })
+// })
+// app.delete("jobs/:jobId", (req, res) => {
+//     const { jobId } = req.params           //{jobId: "dadasdasda"}
+//     res.json({
+//         status: true,
+//         message: "Job Deleted Successfully",
+//     })
+// })
+// app.put("jobs/:jobId", (req, res) => {
+//     const { jobId } = req.params           //{jobId: "dadasdasda"}
+//     res.json({
+//         status: true,
+//         message: "Job Deleted Successfully",
+//     })
+// })
+// app.patch("jobs/:jobId", (req, res) => {
+//     const { jobId } = req.params           //{jobId: "dadasdasda"}
+//     res.json({
+//         status: true,
+//         message: "Job Deleted Successfully",
+//     })
+// })
+// app.post("category", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "Category created Successfully",
+//         data: {}
+//     })
+// })
+// app.get("category", (req, res) => {
+//     res.json({
+//         status: true,
+//         message: "Category get Successfully",
+//         data: []
+//     })
+// })
+// app.get("category/:categoryId", (req, res) => {
+//     const { categoryId } = req.params
+//     res.json({
+//         status: true,
+//         message: "Category get Successfully",
+//         data: []
+//     })
+// })
+// app.put("category/:categoryId", (req, res) => {
+//     const { categoryId } = req.params
+//     res.json({
+//         status: true,
+//         message: "Category get Successfully",
+//         data: []
+//     })
+// })
+// app.patch("category/:categoryId", (req, res) => {
+//     const { categoryId } = req.params
+//     res.json({
+//         status: true,
+//         message: "Category get Successfully",
+//         data: []
+//     })
+// })
+// app.delete("category/:categoryId", (req, res) => {
+//     const { categoryId } = req.params
+//     res.json({
+//         status: true,
+//         message: "Category get Successfully",
+//         data: []
+//     })
+// })
+
+
+
+
+
+
+const PORT = 3000
+
+app.listen(process.env.PORT, () => {
+  console.log(`my server started at port ${PORT}`)
 })
---
+++ routes/authRoute.js
@@ -0,0 +1,1 @@
+const authRoute = samad1
